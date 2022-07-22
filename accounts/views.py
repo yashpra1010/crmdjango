bd9d9ef8f466d241435b57e7be403b5e628e64ff
Yash Prajapati <workwidyash@gmail.com>
2022-07-22 22:57:45 +0530

    update

--- accounts/views.py
+++ accounts/views.py
@@ -112,9 +112,7 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-            Customer.objects.create(user=user,)
+            
             msg.success(request,'Account was created for '+ username)
             return redirect('login')
     context = {'form': form}
