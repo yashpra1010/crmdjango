3070418a5c151e514ae792f7b61b1704a752b443
Yash Prajapati <workwidyash@gmail.com>
2022-07-22 18:46:16 +0530

    update

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lav%%ne=5+u=%wlf4gj*+6+4ay6)!88no8@02-z-rkqeviyfxc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','https://crm-app-yp10.herokuapp.com/']
 
 
 # Application definition
